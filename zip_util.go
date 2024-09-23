dd05cfb729297f754149ea8fa072a53697815aa8
dwd <wenyi173@gmail.com>
2024-09-24 07:35:37 +0800

    add func: UnzipAllAndSaveInDir

--- zip_util.go
+++ zip_util.go
@@ -1,1 +1,23 @@
 package bncvision
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func UnzipAllAndSaveInDir(zipDir string, destDir string) error {
+	files, err := os.ReadDir(zipDir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		zipFilePath := filepath.Join(zipDir, file.Name())
+		err := UnzipAndSave(zipFilePath, destDir)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
