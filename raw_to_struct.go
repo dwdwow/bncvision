b2a6049d7e46823798f149f3c7df7746209f3b51
dwd <wenyi173@gmail.com>
2024-09-29 15:30:38 +0800

    add func: AggTradeRawToStruct

--- raw_to_struct.go
+++ raw_to_struct.go
@@ -41,3 +41,37 @@ func SpotTradeRawToStruct(raw []string) (bnc.SpotTrade, error) {
 	}
 	return trade, nil
 }
+
+func AggTradeRawToStruct(raw []string) (bnc.SpotAggTrades, error) {
+	trade := bnc.SpotAggTrades{}
+	var err error
+	trade.Id, err = strconv.ParseInt(raw[0], 10, 64)
+	if err != nil {
+		return trade, err
+	}
+	trade.Price, err = strconv.ParseFloat(raw[1], 64)
+	if err != nil {
+		return trade, err
+	}
+	trade.Qty, err = strconv.ParseFloat(raw[2], 64)
+	if err != nil {
+		return trade, err
+	}
+	trade.FirstTradeId, err = strconv.ParseInt(raw[3], 10, 64)
+	if err != nil {
+		return trade, err
+	}
+	trade.LastTradeId, err = strconv.ParseInt(raw[4], 10, 64)
+	if err != nil {
+		return trade, err
+	}
+	trade.IsBuyerMaker, err = strconv.ParseBool(raw[5])
+	if err != nil {
+		return trade, err
+	}
+	trade.IsBestMatch, err = strconv.ParseBool(raw[6])
+	if err != nil {
+		return trade, err
+	}
+	return trade, nil
+}
