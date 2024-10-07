cafac309d6627d5bd43ba4f477157f5583adf4cf
dwd <wenyi173@gmail.com>
2024-10-08 03:26:27 +0800

    download

--- cmd/download/spot/one_klines/download_one_syb_klines.go
+++ cmd/download/spot/one_klines/download_one_syb_klines.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/dwdwow/bncvision"
+)
+
+func main() {
+	mid := "spot"
+	timeframe := "1s"
+	symbols := []string{"BTCUSDT"}
+	for _, symbol := range symbols {
+		undownloadContents, err := bncvision.DownloadAllUnderPath("data/"+mid+"/daily/klines/"+symbol+"/"+timeframe, 20)
+		if err != nil {
+			panic(err)
+		}
+		for _, content := range undownloadContents {
+			fmt.Println(content.Key)
+		}
+	}
+}
