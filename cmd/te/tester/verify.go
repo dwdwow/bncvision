62629a21b9628a251d0813d30419ee12aad127da
dwd <wenyi173@gmail.com>
2024-10-24 17:57:17 +0800

    verify

--- cmd/te/tester/verify.go
+++ cmd/te/tester/verify.go
@@ -3,7 +3,7 @@ package tester
 import "github.com/dwdwow/bncvision"
 
 func VerifyOneDirAggTradesContinuity() {
-	dir := "/home/ubuntu/unzip.binance.vision/data/spot/aggTrades/BTCUSDT"
+	dir := "/home/ubuntu/unzip.binance.vision/data/spot/daily/aggTrades/BTCUSDT"
 	maxCpus := 20
 	err := bncvision.VerifyOneDirAggTradesContinuity(dir, maxCpus)
 	if err != nil {
